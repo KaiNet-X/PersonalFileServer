4181dea1f24b4ce8cc96c3b99ed58a9be0b5cb01
KaiNet-X <47905053+KaiNet-X@users.noreply.github.com>
2024-06-19 17:02:25 -0500

    Add avalonia file client

---
+++ FileClient/ViewModels/ConnectionInfoViewModel.cs
@@ -0,0 +1,8 @@
+namespace FileClient.ViewModels;
+using System.Net;
+
+public class ConnectionInfoViewModel : ViewModelBase
+{
+    public string IPAddress address;
+    
+}
---
+++ FileClient/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace FileClient.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
