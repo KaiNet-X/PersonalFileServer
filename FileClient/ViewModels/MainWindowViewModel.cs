e9d26f3011ecf1412f907f2c671e6c1acc4fa774
KaiNet-X <47905053+KaiNet-X@users.noreply.github.com>
2024-06-19 17:02:25 -0500

    Add avalonia file client

---
+++ FileClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FileClient.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+#pragma warning disable CA1822 // Mark members as static
+    public string Greeting => "Hello world";
+#pragma warning restore CA1822 // Mark members as static
+}
