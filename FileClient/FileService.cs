b871d439b0599364648ca5e44d6ffcbf22fbd1d6
kai schellin <ultimatenerdx00@gmail.com>
2024-10-25 16:09:55 -0500

    Implement compression

---
+++ FileClient/FileService.cs
@@ -0,0 +1,100 @@
+﻿namespace FileClient;
+
+using Common;
+using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.IO;
+using System.Threading.Tasks;
+using Net.Connection.Clients.Tcp;
+using System.Threading;
+
+public class FileService
+{
+    private readonly Client client;
+    private readonly AuthService authService;
+
+    private readonly string DownloadDirectory = @$"{Directory.GetCurrentDirectory()}\Files";
+
+    private readonly Dictionary<Guid, MemoryStream> DownloadRequests = new Dictionary<Guid, MemoryStream>();
+
+    private readonly SemaphoreSlim downloadSemaphore = new SemaphoreSlim(1);
+
+    public FileService(Client client, AuthService authService)
+    {
+        this.client = client;
+        this.authService = authService;
+
+        client.OnMessageReceived<FileRequestMessage>(msg => OnFileMessage(msg));
+    }
+
+    public async Task UploadFileAsync(string path, string name)
+    {
+        byte[] memBuf;
+
+        var fileStream = new FileStream(path, new FileStreamOptions
+        {
+            Access = FileAccess.Read,
+            Mode = FileMode.Open,
+            Options = FileOptions.SequentialScan
+        });
+
+        await using (var memoryStream = new MemoryStream())
+        {
+            await using var compressionStream = new GZipStream(memoryStream, CompressionLevel.SmallestSize);
+            await fileStream.CopyToAsync(compressionStream);
+            memBuf = memoryStream.ToArray();
+        }
+
+        if (memBuf.Length > fileStream.Length)
+        {
+            memBuf = new byte[fileStream.Length];
+            fileStream.Seek(0, SeekOrigin.Begin);
+            await fileStream.ReadAsync(memBuf);
+        }
+
+        await fileStream.DisposeAsync();
+
+        var newMsg = new FileRequestMessage() { RequestType = FileRequestType.Upload, PathRequest = name, User = authService.User, FileData = memBuf };
+       
+        await client.SendMessageAsync(newMsg);
+    }
+
+    private async Task OnFileMessage(FileRequestMessage msg)
+    {
+        Directory.CreateDirectory(DownloadDirectory);
+
+        if (!DownloadRequests.ContainsKey(msg.RequestId))
+            DownloadRequests.Add(msg.RequestId, new MemoryStream());
+
+        var current = DownloadRequests[msg.RequestId];
+
+        try
+        {
+            await downloadSemaphore.WaitAsync();
+            await current.WriteAsync(msg.FileData);
+        }
+        catch (Exception ex)
+        {
+
+        }
+        finally
+        {
+            downloadSemaphore.Release();
+        }
+
+        if (msg.EndOfMessage)
+        {
+            current.Seek(0, SeekOrigin.Begin);
+
+            await using (var newFile = File.Create($@"{DownloadDirectory}\{msg.FileName}"))
+            {
+                await using (var decompress = new GZipStream(current, CompressionMode.Decompress))
+                {
+                    await decompress.CopyToAsync(newFile);
+                }
+            }
+            DownloadRequests.Remove(msg.RequestId);
+        }
+    }
+}
---
+++ FileClient/Views/FileTree.axaml.cs
@@ -0,0 +1,84 @@
+namespace FileClient.Views;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Common;
+using FileClient.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Net.Connection.Clients.Tcp;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.IO.Compression;
+
+public partial class FileTree : UserControl
+{
+    public static readonly DirectProperty<FileTree, ObservableCollection<Node>> NodesProperty =
+        AvaloniaProperty.RegisterDirect<FileTree, ObservableCollection<Node>>(
+            nameof(Nodes),
+            c => c.Nodes,
+            (c, val) => c.Nodes = val,
+            defaultBindingMode: Avalonia.Data.BindingMode.TwoWay);
+
+    private readonly AuthService authService;
+    private readonly FileService fileService;
+    private readonly Client client;
+
+    private ObservableCollection<Node> _nodes;
+    public ObservableCollection<Node> Nodes 
+    { 
+        get => _nodes;
+        set => SetAndRaise(NodesProperty, ref _nodes, value);
+    }
+
+    public Node SelectedNode { get; set; }
+
+    public FileTree()
+    {
+        InitializeComponent();
+        authService = Extensions1.ServiceProvider.GetService<AuthService>();
+        client = Extensions1.ServiceProvider.GetService<Client>();
+        fileService = Extensions1.ServiceProvider.GetService<FileService>();
+
+        DataContext = this;
+    }
+
+    public async void Upload(object sender, RoutedEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        var picker = await topLevel.StorageProvider.OpenFilePickerAsync(new Avalonia.Platform.Storage.FilePickerOpenOptions
+        {
+            AllowMultiple = false
+        });
+
+        if (picker.Count > 0)
+            await fileService.UploadFileAsync(picker[0].Path.AbsolutePath, picker[0].Name);
+    }
+
+    public async void Download(object sender, RoutedEventArgs e)
+    {
+        if (SelectedNode == null)
+            return;
+        
+        await client.SendMessageAsync(new FileRequestMessage { RequestType = FileRequestType.Download, PathRequest = SelectedNode.Title, User = authService.User });
+    }
+}
+
+public class Node
+{
+    public ObservableCollection<Node>? SubNodes { get; set; }
+    public string Title { get; }
+
+    public Node(string title)
+    {
+        Title = title;
+    }
+
+    public Node(string title, ObservableCollection<Node> subNodes)
+    {
+        Title = title;
+        SubNodes = subNodes;
+    }
+}
